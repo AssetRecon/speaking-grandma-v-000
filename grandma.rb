af5be0abc24c989d2d36a58c01c77c31ad15ee62
Edsel Caprice <caprice@mail.usf.edu>
2018-06-16 23:32:01 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -8,7 +8,7 @@ if "I LOVE YOU GRANDMA!" == phrase
   elsif phrase == phrase.upcase
   return "NO, NOT SINCE 1938!"
 else
-  return "HUH?! SPEAK UP, SONY!"
+  return "HUH?! SPEAK UP, SONNY!"
 end
 end
 
